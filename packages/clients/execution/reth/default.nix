eda4c118dca197a28ce333d1a8df00e0e94e7ad6
georgewhewell <georgerw@gmail.com>
2023-07-19 13:27:52 +0200

    feat(reth): use main branch

--- packages/clients/execution/reth/default.nix
+++ packages/clients/execution/reth/default.nix
@@ -7,21 +7,21 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "reth";
-  version = "0.1.0-alpha.2";
+  version = "0.1.0-alpha.4";
 
   src = fetchFromGitHub {
     owner = "paradigmxyz";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-+kGhVL7sIbd3WBaRA6XMTAPMCkxRTRacQKFM8oOCqg0=";
+    rev = "bdb23b3703bf495cb308defa433fc9b0ef283408";
+    hash = "sha256-19BjGRK4z7bSfa9YS8ej2CPjmRVVxxb1pZ05IZbLgUI=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     allowBuiltinFetchGit = true;
     outputHashes = {
-      "boa_ast-0.16.0" = "sha256-M4tDrKM+cR3Xc7qZQ5uHw7lsMEP6OG/VvyHC2oW0BrI=";
-      "discv5-0.3.0" = "sha256-Z1UZY47C2qtEr4WrOEiWynzsiwggOOEy9slZO5n97BM=";
+      "boa_ast-0.17.0" = "sha256-PPmDXyu0eo+aX3USMBjmQx0kIkJeW+Y3o/gGQ+dnjok=";
+      "discv5-0.3.0" = "sha256-f+GfS6+lB5aIgQ08nGajljYOdHTbOj0EL8Bburyo/ao=";
       "igd-0.12.0" = "sha256-wjk/VIddbuoNFljasH5zsHa2JWiOuSW4VlcUS+ed5YY=";
       "revm-3.3.0" = "sha256-jmDzHpbWTXxkv+ATAqYznvcQy8V3EF2XVsCyLaH4p0o=";
       "ruint-1.8.0" = "sha256-OzIUivkNwtox7cMdqv6tkCMsJsGyVeTvfyMr5SZhuPg=";
